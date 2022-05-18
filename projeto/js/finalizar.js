af5adce0ac05c42e88cc9fb355490ac535c69a4d
KaykeAmorim <kaykeamorim6@gmail.com>
2022-05-17 22:14:48 -0300

    Projeto v.1

--- projeto/js/finalizar.js
+++ projeto/js/finalizar.js
@@ -1,21 +1,18 @@
 var formE = document.forms.formEnvio;
-let listaDeMesas = JSON.parse(localStorage.mesas)
-let mesaSelecionada = localStorage.mesaSelecionada
-let mesa = listaDeMesas.filter(lista => lista.nome == mesaSelecionada)
-let produtosAdquiridos = mesa[0].produto
+let produtoJSON = JSON.parse(localStorage.getItem("produtoFinalizado"))
 
 var idProduto = document.getElementById("idProduto");
-idProduto.value = Math.floor((Math.random()*1000000000));
+idProduto.value = produtoJSON.id
 var textArea = document.getElementById("produtos");
 var input = document.getElementById("mesa");
 var progress = document.getElementById("progress")
 var statusPedido = document.getElementById("status");
-statusPedido.value = "PENDENTE";
-input.value = mesaSelecionada;
+statusPedido.value = "ENTREGUE";
+input.value = produtoJSON.nome;
 
-for(var i = 0; i < produtosAdquiridos.length; i++){
-    textArea.innerHTML += produtosAdquiridos[i].qtd + "x " + produtosAdquiridos[i].titulo + '\n'
-}
+
+textArea.innerHTML = produtoJSON.descricao;
+
 
 var btnEnviar = document.getElementById("btnEnviar").onclick=function(){
     if(verificaSeVazio(input.value, "Preencha o nome da mesa!"));
@@ -23,6 +20,7 @@ var btnEnviar = document.getElementById("btnEnviar").onclick=function(){
     else{
         progress.setAttribute("style", "width:100%")
         formE.submit();
+        location.href="cozinha.html"
     }
 }
 
